34a99bf8a1c2fe77f765123c9143b41e7838760e
Екатерина <140340116+RuzKate@users.noreply.github.com>
2024-07-17 15:46:50 +0300

    Update extract.py

--- scripts/extract.py
+++ scripts/extract.py
@@ -2,21 +2,19 @@ import pandas as pd
 
 class ExtractData:
     def __init__(self, table_name, code):
+        """Инициализатор класса
+        
+            :param table_name: название таблицы
+            :type table_name: str
+
+            :param code: кодировка CSV-файла
+            :type code: str
+        """
         self.path_file = f'csv_file/{table_name}.csv'
         self.code = code
         self.data = None
 
 
     def extract_data(self):
-        """Извлечение данных
-
-            :param path_file: путь к файлу
-            :type path_file: str
-
-            :param code: кодировка файла
-            :type code: str
-
-            :rtype: DataFrame
-            :return: извлеченные данные из csv файла
-        """
+        """Извлечение данных из CSV-файла"""
         self.data = pd.read_csv(self.path_file, sep=';', encoding=self.code, na_filter=False) 
